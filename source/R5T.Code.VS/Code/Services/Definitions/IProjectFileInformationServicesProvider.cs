23bed0012c95626654f9b30916c6622a83be7367
davidcoats <david@getsafetycone.com>
2022-04-27 15:26:05 -0700

    Interim changes.

---
+++ source/R5T.Code.VS/Code/Services/Definitions/IProjectFileInformationServicesProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.Code.VisualStudio.IO;
+using R5T.Tools.NuGet;
+
+using R5T.T0064;
+
+
+namespace R5T.Code.VisualStudio
+{
+    [ServiceDefinitionMarker]
+    public interface IProjectFileInformationServicesProvider : IServiceDefinition
+    {
+        ProjectFilePath[] GetProjectReferenceFilePaths(ProjectFilePath projectFilePath);
+        ProjectFilePath[] GetProjectReferencedFilePathsRecursive(ProjectFilePath projectFilePath);
+        PackageSpecification[] GetPackageReferences(ProjectFilePath projectFilePath);
+
+        /// <summary>
+        /// Determines if a project file has a version, and if so, returns the version.
+        /// If the project file does not have a version, returns <see cref="R5T.NetStandard.VersionHelper.None"/>.
+        /// </summary>
+        bool HasVersion(ProjectFilePath projectFilePath, out Version version);
+    }
+}
---
+++ source/R5T.Code.VS/Code/Services/Definitions/ISolutionFileActionServicesProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using R5T.Code.VisualStudio.IO;
+
+using R5T.T0064;
+
+
+namespace R5T.Code.VisualStudio
+{
+    /// <summary>
+    /// Service for creating Visual Studio solution files.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface ISolutionFileActionServicesProvider : IServiceDefinition
+    {
+        /// <summary>
+        /// Adds a project-file to a solution-file.
+        /// </summary>
+        void AddProject(SolutionFilePath solutionFilePath, ProjectFilePath projectFilePath);
+
+        void RemoveProject(SolutionFilePath solutionFilePath, ProjectFilePath projectFilePath);
+
+        /// <summary>
+        /// Creates a solution-file at the specified path.
+        /// </summary>
+        void CreateSolutionFile(SolutionFilePath solutionFilePath);
+    }
+}
